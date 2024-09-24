c766e4af3c46c805689852b9e915b5da8b6d5753
Ferxi14 <fernandogr0309@gmail.com>
2024-09-24 09:28:00 +0200

    ej1b hecho

---
+++ ej1b.php
@@ -0,0 +1,21 @@
+<HTML>
+<HEAD><TITLE> EJ1B – Conversor decimal a binario</TITLE></HEAD>
+<BODY>
+<?php
+$num="168";
+$numAux = $num;
+$binario = "";
+
+while ($numAux > 0){
+    $resto = $numAux % 2;
+    $binario = $binario . $resto;
+
+
+    $numAux = (int)$numAux/2; 
+}
+
+echo "numero $num en binario: $binario";
+
+?>
+</BODY>
+</HTML>
