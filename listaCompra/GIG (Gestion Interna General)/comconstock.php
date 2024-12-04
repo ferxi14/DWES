81db7775c3fe8411117701e4d95519471ccd61cc
Ferxi14 <fernandogr0309@gmail.com>
2024-12-04 10:52:29 +0100

    .

--- listaCompra/GIG (Gestion Interna General)/comconstock.php
+++ listaCompra/GIG (Gestion Interna General)/comconstock.php
@@ -4,5 +4,21 @@ $username = "root";
 $password = "rootroot";
 $dbname = "comprasweb";
 
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT id_producto, nombre FROM PRODUCTO");
+    $stmt->execute();
+    $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $id_producto = $_POST['id_producto'];
+
+        
+    }
+} catch (PDOException $e) {
+    echo "Conexión fallida: " . $e->getMessage();
+}
 
 ?>
