967b875c76c961078041ca2c2b3f6a03eeb46bde
Ferxi14 <fernandogr0309@gmail.com>
2024-12-10 09:36:50 +0100

    pagina de ejemplo para ver si funciona el login

--- webPedidos/pe_inicio.php
+++ webPedidos/pe_inicio.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal de la web</title>
+    <title>Portal de la web de pedidos</title>
 </head>
 <body>
     <div>
